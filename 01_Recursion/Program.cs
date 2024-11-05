0d501e6e0f42667795b6c864607b2b926b573ee4
Axel Vazquez <axelvazmar0@gmail.com>
2024-11-05 07:23:12 -0600

    conociendo la recursión por medio de los factoriales

---
+++ 01_Recursion/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int f = 0;
+        //Factorial de 5! = 5*4*3*2*1
+        f = Factorial(1);
+        Console.WriteLine(f);
+
+    }
+
+    public static int Factorial(int n)
+    {
+        int r = 0;
+        // Caso inductivo
+        if (n > 1)
+        {
+            r = n * Factorial(n - 1);
+        }
+
+        if (n == 1)
+        {
+            r = 1;
+        }
+
+        return r;
+    }
+}
+
